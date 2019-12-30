1f81ebf516e3a14f33bea55ccae94b0f9086e51d
Chris Cheong <chrisxcheong@gmail.com>
2019-12-30 05:13:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,9 +28,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # title == "Home Alone"
   # release_date == 1990
 
-  Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = "1990"
+  Movie.create(args) do |m|
   end
 end
 
